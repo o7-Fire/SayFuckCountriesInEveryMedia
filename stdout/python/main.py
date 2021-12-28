07b2331cb1af30481726964959ff8fd4d4606954
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-12-28 03:26:12 +0000

    get looped

--- stdout/python/main.py
+++ stdout/python/main.py
@@ -2,4 +2,5 @@ import sys
 import random
 
 countries = open("../../countries.txt", "r").read().split("\n")
-sys.stdout.write(f"Fuck {random.choice(countries)}")
+while True:
+  sys.stdout.write(f"Fuck {random.choice(countries)}")
