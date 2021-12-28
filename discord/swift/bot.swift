8fe61405678135fa574cd6496bb52e7ca6a097e2
shadow01148 <76457846+shadow01148@users.noreply.github.com>
2021-12-28 20:31:46 +0800

    Update bot.swift

--- discord/swift/bot.swift
+++ discord/swift/bot.swift
@@ -1,21 +1,21 @@
-// I don't get paid enough to test this
+# https://github.com/Azoy/Sword
 
 import Sword
 
-let bot = Sword(token: "TOKEN")
+let bot = Sword(token: "TOKENHERE")
 
 bot.on(.messageCreate) { data in
   let msg = data as! Message
 
   if msg.content == "Fuck" {
-    let file = "countries"
-    if let path = Bundle.main.path(forResource: file, ofType: "txt"){
+      let file = "countries.txt"
+      if let path = Bundle.main.path(forResource: file, ofType: "txt"){
     do {
         let data = try String(contentsOfFile: path, encoding: .utf8)
         let myStrings = data.components(.split { $0.IsNewline })
         let text = myStrings.joined(separator: "\n")
-        msg.reply(text: text)
-  }
+        msg.reply(with: "Fuck" + text)
+    }
 }
 
 bot.connect()
