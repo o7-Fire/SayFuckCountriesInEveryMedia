ea2e12670866ceb6c223c000d49f18b0b950218b
shadow01148 <76457846+shadow01148@users.noreply.github.com>
2021-12-28 16:36:01 +0800

    Create bot.clj

---
+++ discord/Clojure/bot.clj
@@ -0,0 +1,33 @@
+(require '[clojure.core.async    :as a])
+(require '[discljord.connections :as c])
+(require '[discljord.messaging   :as m])
+
+(def token      "TOKEN")
+(def intents    #{:guilds :guild-messages})
+(def channel-id "12345")
+
+(let [event-ch      (a/chan 100)
+      connection-ch (c/connect-bot! token event-ch :intents intents)
+      message-ch    (m/start-connection! token)]
+  (try
+    (loop []
+      (let [[event-type event-data] (a/<!! event-ch)]
+        (when (and (= :message-create event-type)
+                   (= (:channel-id event-data) channel-id)
+                   (not (:bot (:author event-data))))
+          (let [message-content (:content event-data)]
+            (if (= "Fuck" (s/trim (s/lower-case message-content)))
+              (do
+                (use 'clojure.java.io)
+                (with-open [rdr (reader "countries.txt")]
+                (doseq [line (line-seq rdr)]
+                (m/create-message! message-ch channel-id ::content line)))
+                (c/disconnect-bot! connection-ch))
+              (m/create-message! message-ch channel-id :content message-content))))
+        (when (= :channel-pins-update event-type)
+          (c/disconnect-bot! connection-ch))
+        (when-not (= :disconnect event-type)
+          (recur))))
+    (finally
+      (m/stop-connection! message-ch)
+      (a/close!           event-ch))))
