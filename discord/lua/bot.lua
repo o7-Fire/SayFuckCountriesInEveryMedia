1754d39650e1386e7ff91218d7ff68c9113cae5a
shadow01148 <76457846+shadow01148@users.noreply.github.com>
2021-12-28 19:46:43 +0800

    update update update
    better now
    
    do not merge this yet

--- discord/lua/bot.lua
+++ discord/lua/bot.lua
@@ -1,17 +1,18 @@
-local discordia = require('discordia')
+-- luvit bot.lua
+-- lit install SinisterRectus/discordia
 
+local discordia = require('discordia')
+local open = io.open
 local client = discordia.Client()
 
-
-client:on('messageCreate', function(message, filename, fp, io)
+client:on('messageCreate', function(message)
 	if message.content == 'Fuck' then
-        filename = "countries.txt"
-        fp = io.open( filename, "r" )
-           for line in fp:lines() do
-            message.channel:sendMessage(line)
-         end
-      end
-  end)
-
+    file = io.open("countries.txt", "r")
+     for line in fp:lines() do 
+      message.channel:sendMessage('Fuck'.. line)
+    end
+    file:close()
+  end
+end)
 
 client:run('TOKEN')
