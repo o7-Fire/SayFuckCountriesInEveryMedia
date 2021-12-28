35d86e0334864939643aa0a48b617a4864c2c73e
shadow01148 <76457846+shadow01148@users.noreply.github.com>
2021-12-28 08:45:58 +0800

    Create main.py
    im not very good at this

---
+++ discord/python/bot/main.py
@@ -0,0 +1,15 @@
+import discord 
+import random
+
+TOKEN = 'TOKEN'
+
+client = discord.Client()
+
+@client.event 
+async def on_message(message):
+    if message.content.startswith('Fuck'):
+        countries = open("countries.txt", "r").read().split("\n")
+        country = random.choice(countries)
+        await message.channel.send(country)
+
+client.run(TOKEN)
