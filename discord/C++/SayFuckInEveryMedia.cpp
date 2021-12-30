e08d19454152c93475a2c2eecd23b847329118e7
shadow01148 <76457846+shadow01148@users.noreply.github.com>
2021-12-30 14:38:21 +0800

    Create SayFuckInEveryMedia.cpp

---
+++ discord/C++/SayFuckInEveryMedia.cpp
@@ -0,0 +1,27 @@
+// dpp.dev
+
+#include <dpp/dpp.h>
+#include <fstream>
+
+int main() {
+    dpp::cluster bot("TOKEN");
+
+    bot.on_ready([&bot](const auto& event) {
+        std::cout << "Logged in as " << bot.me.username << "!\n";
+    });
+
+    bot.on_message_create([&bot](const dpp::message_create_t& event) {
+        if (event.msg.content == "test") {
+            std::string line;
+            std::ifstream myfile;
+            myfile.open("countries.txt");
+            while (getline(myfile, line)) {
+                dpp::message msg(event.msg.channel_id, line);
+            }
+        }
+    });
+    bot.start(false); // change this to true 
+    return 0;
+}
+
+// who knows if it's gonna work
