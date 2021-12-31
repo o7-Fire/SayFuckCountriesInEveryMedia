78a34ef169e1efb9da89089803dfdb22e66a8a7c
shadow01148 <76457846+shadow01148@users.noreply.github.com>
2021-12-31 18:51:17 +0800

    Create main.ps1
    powershell who

---
+++ twitter/Powershell/main.ps1
@@ -0,0 +1,10 @@
+# Install-Module -Name BlueBirdPS -Scope CurrentUser
+# Set-TwitterAuthentication
+# Powershell 7 is a must
+
+
+foreach($line in Get-Content .\countries.txt) {
+    if($line -match $regex){
+        Publish-Tweet -TweetText "Fuck" + $line
+    }
+}
