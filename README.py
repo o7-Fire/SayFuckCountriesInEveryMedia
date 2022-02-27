d2527009153c5e9d234e5a9b75ad7ef397655083
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2022-02-27 06:50:27 +0100

    kys

--- README.py
+++ README.py
@@ -1,8 +1,8 @@
 import os
-from pathlib import Path
 import re
+from pathlib import Path
 
-
+# the fukc ?
 CWD = Path(os.getcwd())
 PRE_KOMI = '''
 python3 ../../README.py
@@ -10,7 +10,7 @@ git add ../../README.md
 echo TABLE GENERATED
 '''
 
-
+# what is this?
 HOOK = CWD / '.git/hooks/pre-commit'
 if HOOK.exists():
     hook = HOOK.read_text()
@@ -19,10 +19,10 @@ if HOOK.exists():
 else:
     HOOK.write_text('#!/bin/sh\n' + PRE_KOMI)
 
-
 rm = Path('README.md')
 langs = set()
 plats = {}
+orig = rm.read_text()
 
 
 for p in CWD.iterdir():
@@ -53,4 +53,17 @@ table += '\n</AUTOMATED>'
 
 ctx = rm.read_text('utf-8')
 ctx = re.sub('<AUTOMATED>.+</AUTOMATED>', table, ctx, flags=re.S)
-rm.write_text(ctx, 'utf-8')
+if ctx != orig:
+    rm.write_text(ctx, 'utf-8')
+    print('TABLE GENERATED')
+else:
+    print("TABLE UP TO DATE")
+    exit(0)
+
+if os.getlogin() != "runner":
+    print("Not runner, skipping git commit")
+    exit(0)
+
+os.system('git add README.md')
+os.system('git commit -m "Matrix Update"')
+os.system('git push')
