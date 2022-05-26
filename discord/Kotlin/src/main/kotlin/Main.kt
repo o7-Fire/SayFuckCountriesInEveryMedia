d61e6d60b3d1885a09f8ed758325e5c230a5b185
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2022-05-26 14:19:33 +0200

    mf failed

--- discord/Kotlin/src/main/kotlin/Main.kt
+++ discord/Kotlin/src/main/kotlin/Main.kt
@@ -1,7 +1,32 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import dev.kord.core.Kord
+import dev.kord.core.entity.ReactionEmoji
+import dev.kord.core.event.gateway.ReadyEvent
+import dev.kord.core.event.message.MessageCreateEvent
+import dev.kord.core.on
+import dev.kord.gateway.Intent
+import dev.kord.gateway.PrivilegedIntent
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.io.File
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+suspend fun main(args: Array<String>) {
+
+
+    val countries = File("countries.txt").readLines()
+
+    val kord: Kord = Kord(System.getenv("DISCORD_TOKEN"))
+    kord.on<MessageCreateEvent> {
+        if (message.content != "fuck") return@on
+        for(country in countries) {
+            val join = launch {   message.channel.createMessage("fuck $country")}
+            join.join()
+        }
+    }
+    kord.on<ReadyEvent> {
+        println("Ready! ${this.self.username}#${this.self.discriminator}")
+    }
+    kord.login {
+        @OptIn(PrivilegedIntent::class)
+        intents += Intent.MessageContent
+    }
 }
