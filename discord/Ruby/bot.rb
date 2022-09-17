9e07a004eea12fd191790a949ce0c180f83f0a61
Robert <76457846+shadow01148@users.noreply.github.com>
2022-09-17 23:07:15 +0800

    Update bot.rb
    no print

--- discord/Ruby/bot.rb
+++ discord/Ruby/bot.rb
@@ -5,7 +5,6 @@ bot = Discordrb::Bot.new token: 'TOKEN'
 bot.message(with_text: 'Fuck') do |event|
   f = File.open("countries.txt", "r")
   f.each_line do |line|
-  puts line
   event.respond 'Fuck' line
 end
 
