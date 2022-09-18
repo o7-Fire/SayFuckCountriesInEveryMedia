2e91dfab871441e15012dc46364e1d1046773599
Robert <76457846+shadow01148@users.noreply.github.com>
2022-09-18 15:26:49 +0800

    its better

--- stdout/Powershell/main.ps1
+++ stdout/Powershell/main.ps1
@@ -1,6 +1,6 @@
 foreach($line in Get-Content .\countries.txt) {
     if($line -match $regex){
-        Write-Output "Fuck" + $line
+        Write-Output "Fuck $line"
         Start-Sleep -Seconds 2
     }
 }
