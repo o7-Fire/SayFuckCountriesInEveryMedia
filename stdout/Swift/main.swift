2a948129f4223ba94c5695b6bf5a697775d3c85d
Robert <76457846+shadow01148@users.noreply.github.com>
2022-09-19 14:54:10 +0800

    Create main.swift

---
+++ stdout/Swift/main.swift
@@ -0,0 +1,7 @@
+let file = "countries.txt"
+if let path = Bundle.main.path(forResource: file, ofType: "txt"){
+    do {
+        let data = try String(contentsOfFile: path, encoding: .utf8)
+        let myStrings = data.components(.split { $0.IsNewline })
+        let text = myStrings.joined(separator: "\n")
+        print("Fuck" + text)
