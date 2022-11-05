4a6c48f439d4fb09b39c7f004d36846535b67a8f
Robert <76457846+shadow01148@users.noreply.github.com>
2022-11-05 22:17:03 +0800

    Update bot.c

--- discord/c/bot.c
+++ discord/c/bot.c
@@ -40,7 +40,7 @@ void on_message(struct discord *client, const struct discord_message *event) {
 
  
 int main(void) {
-  struct discord *client = discord_init("TOKEN");
+  struct discord *client = discord_init(getenv("TOKEN"));
   discord_add_intents(client, DISCORD_GATEWAY_MESSAGE_CONTENT);
   discord_set_on_ready(client, &on_ready);
   discord_set_on_message_create(client, &on_message);
